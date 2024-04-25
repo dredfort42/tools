1d344df60c5d858a57858d2a528776af88ca8fa2
Dmitry Novikov <dredfort.42@gmail.com>
2024-04-25 11:12:53 +0100

    DEBUG printer added

--- logprinter/logprinter.go
+++ logprinter/logprinter.go
@@ -2,6 +2,7 @@ package logprinter
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -9,6 +10,7 @@ import (
 const (
 	RED    = "\033[1;31m"
 	GREEN  = "\033[1;32m"
+	BLUE   = "\033[1;34m"
 	YELLOW = "\033[1;33m"
 	RESET  = "\033[0m"
 )
@@ -18,38 +20,59 @@ func getCurrentTime() string {
 	return time.Now().UTC().Format("2006-01-02 15:04:05")
 }
 
-// PrintSuccess prints a success message
-func PrintSuccess(msg string, info string) {
-	if info == "" {
-		fmt.Printf("%s %s[S] %s%s\n", getCurrentTime(), GREEN, msg, RESET)
-	} else {
-		fmt.Printf("%s %s[S] %s: %s%s\n", getCurrentTime(), GREEN, msg, info, RESET)
+// Success prints a success message
+func Success(msg string, info ...string) {
+	fmt.Printf("%s %s[S] %s", getCurrentTime(), GREEN, msg)
+
+	for i := 0; i < len(info); i++ {
+		fmt.Printf(" | %s", info[i])
 	}
 }
 
-// PrintWarning prints a warning message
-func PrintWarning(msg string, info string) {
-	if info == "" {
-		fmt.Printf("%s %s[W] %s%s\n", getCurrentTime(), YELLOW, msg, RESET)
-	} else {
-		fmt.Printf("%s %s[W] %s: %s%s\n", getCurrentTime(), YELLOW, msg, info, RESET)
+// Warning prints a warning message
+func Warning(msg string, info ...string) {
+	fmt.Printf("%s %s[W] %s", getCurrentTime(), YELLOW, msg)
+
+	for i := 0; i < len(info); i++ {
+		fmt.Printf(" | %s", info[i])
 	}
+
+	fmt.Printf("%s\n", RESET)
 }
 
-// PrintError prints an error message
-func PrintError(msg string, err error) {
-	if err == nil {
-		fmt.Printf("%s %s[E] %s%s\n", getCurrentTime(), RED, msg, RESET)
-	} else {
-		fmt.Printf("%s %s[E] %s: %s%s\n", getCurrentTime(), RED, msg, err.Error(), RESET)
+// Error prints an error message
+func Error(msg string, err error) {
+	fmt.Printf("%s %s[E] %s", getCurrentTime(), RED, msg)
+
+	if err != nil {
+		fmt.Printf(" | %s", err.Error())
 	}
+
+	fmt.Printf("%s\n", RESET)
 }
 
-// PrintInfo prints an info message
-func PrintInfo(msg string, info string) {
-	if info == "" {
-		fmt.Printf("%s [I] %s\n", getCurrentTime(), msg)
-	} else {
-		fmt.Printf("%s [I] %s: %s\n", getCurrentTime(), msg, info)
+// Info prints an info message
+func Info(msg string, info ...string) {
+	fmt.Printf("%s [I] %s", getCurrentTime(), msg)
+
+	for i := 0; i < len(info); i++ {
+		fmt.Printf(" | %s", info[i])
 	}
+
+	fmt.Println()
+}
+
+// Debug prints a debug message if the DEBUG environment variable is set to "1"
+func Debug(msg string, info ...string) {
+	if os.Getenv("DEBUG") != "1" {
+		return
+	}
+
+	fmt.Printf("%s %s[D] %s%s", getCurrentTime(), BLUE, RESET, msg)
+
+	for i := 0; i < len(info); i++ {
+		fmt.Printf(" | %s", info[i])
+	}
+
+	fmt.Println()
 }
